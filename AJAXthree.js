f3d0de765662f2a491040f67180d166da2a97604
zach <62062622+zach12904@users.noreply.github.com>
2022-01-19 10:36:26 -0500

    Add files via upload

---
+++ AJAXthree.js
@@ -0,0 +1,13 @@
+const request = new XMLHttpRequest();
+
+request.addEventListener("readystagechange", () => {
+  //console.log(request, request.readyState);
+  if(request.readyState === 4 && request.status === 200){
+    console.log(request, request.responseText);
+  } else if(request.readyState === 4){
+    console.log('could not fetch the data');
+  }
+});
+
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+request.send();
